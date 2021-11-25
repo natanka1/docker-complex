83f3d6dc17b76147678409309ffcc5f56e9d7dee
natanka <natanka@rafael.co.il>
2021-11-25 19:20:57 +0200

    added git ignore,

---
+++ client/src/Fib.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class Fib extends Component {
+    state = {
+        seenIndexes: [],
+        values: {},
+        index: ''
+    };
+
+    componentDidMount() {
+        this.fetchValues();
+        this.fetchIndexes();
+    }
+
+    async fetchValues() {
+        const values = await axios.get('/api/values/current');
+    }
+}
